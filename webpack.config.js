baaaed6e7a563c279066472706bced592ac2d8f6
Du Than <xopxop.1403@gmail.com>
2026-08-22 09:40:17 +0300

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+};
